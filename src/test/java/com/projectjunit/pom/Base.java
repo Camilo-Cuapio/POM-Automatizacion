4001ad151569de8502916eb5517215a29e62e3b4
Camilo-Cuapio <cuapio.camilo@gmail.com>
2026-01-25 07:12:14 -0600

    Se empieza a realizar repaso

--- src/test/java/com/projectjunit/pom/Base.java
+++ src/test/java/com/projectjunit/pom/Base.java
@@ -9,55 +9,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 public class Base {
+
     private WebDriver driver;
 
-    public Base(WebDriver driver) {
-        this.driver = driver;
+    public Base(WebDriver driver){
+        this.driver=driver;
 
     }
 
-    public WebDriver chromeDriverConnection() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        return driver;
-    }
-
-    public WebElement finElement(By locator) {
-        return driver.findElement(locator);
-    }
-
-    public List<WebElement> findElements(By locator) {
-        return driver.findElements(locator);
-    }
-
-    public String getText(WebElement element) {
-        return element.getText();
-    }
-
-    public String getText(By locator) {
-        return driver.findElement(locator).getText();
-    }
-
-    public void type(String inputText, By locator) {
-        driver.findElement(locator).sendKeys(inputText);
-    }
-
-    public void click(By locator) {
-        driver.findElement(locator).click();
-    }
-
-    public Boolean isDisplayed(By locator) {
-        try {
-            return driver.findElement(locator).isDisplayed();
-        } catch (org.openqa.selenium.NoSuchElementException e) {
-            return false;
-            //Todo:handle exception
-
-        }
-    }
-
-    public void visit(String url) {
-        driver.get(url);
-    }
 }
 
