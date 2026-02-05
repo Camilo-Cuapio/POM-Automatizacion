3cdefb3d2047519765fa1d0564a3490e5689f26b
Camilo-Cuapio <cuapio.camilo@gmail.com>
2026-02-05 13:41:08 -0600

    llevar cambios a main

--- src/test/java/paginaproductos/pom/IniciarSesionPagina.java
+++ src/test/java/paginaproductos/pom/IniciarSesionPagina.java
@@ -1,9 +1,11 @@
 package paginaproductos.pom;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class IniciarSesionPagina extends Base {
@@ -11,13 +13,13 @@ public class IniciarSesionPagina extends Base {
     By usuarioLocalizador = By.xpath("//*[@id=\"user-name\"]");
     By passLocalizador = By.cssSelector("#password");
     By loginBtnLocalizador = By.cssSelector(".submit-button");
-//Mensaje de error, contraseña incorrecta
+    //Mensaje de error, contraseña incorrecta
     By errorContrasenaMensaje = By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3");
-//Localizadores de X
-    By xUsuario=By.cssSelector("#login_button_container>div>form>div:nth-child(1)>svg");
-    By xPassword=By.cssSelector("#login_button_container>div>form>div:nth-child(2)>svg");
-    By xMsjError=By.cssSelector(".error-message-container>h3>button>svg");
-
+    //Localizadores de X
+    By xUsuario = By.cssSelector("#login_button_container>div>form>div:nth-child(1)>svg");
+    By xPassword = By.cssSelector("#login_button_container>div>form>div:nth-child(2)>svg");
+    By xMsjError = By.cssSelector(".error-message-container>h3>button>svg");
+    By listadeProductosElement = By.cssSelector(".inventory_item_name ");
 
     public IniciarSesionPagina(WebDriver driver) {
         super(driver);
@@ -48,7 +50,8 @@ public class IniciarSesionPagina extends Base {
             System.out.println("No se realizo el logeo correctamente");
         }
     }
-    public String mensajeErrorLogeo(){
+
+    public String mensajeErrorLogeo() {
 
         return getText(errorContrasenaMensaje);
 
@@ -56,17 +59,34 @@ public class IniciarSesionPagina extends Base {
 
     //Validacion de X en Usuario, Password y MsjError
 
-    public int xUsu(){
-     List< WebElement > xUsu=findElements(xUsuario);
-     return xUsu.size();
-    }
-    public int xPass(){
-        List<WebElement>xPass=findElements(xPassword);
-        return xPass.size();
-    }
-    public int xMsjErrorr(){
-        List<WebElement>xMsjErrorr=findElements(xMsjError);
-        return xMsjErrorr.size();
+    public int xUsu() {
+        List<WebElement> xUsu = findElements(xUsuario);
+        return xUsu.size();
     }
 
+    public int xPass() {
+        List<WebElement> xPass = findElements(xPassword);
+        return xPass.size();
+    }
+
+    public int xMsjErrorr() {
+        List<WebElement> xMsjErrorr = findElements(xMsjError);
+        return xMsjErrorr.size();
+    }
+    //Obtener actuales productos
+
+    public List<String> actualProductoss() {
+        List<WebElement> actualProductoselement = findElements(listadeProductosElement);
+        List<String> actualProductos = new ArrayList<>();
+
+        for (WebElement element : actualProductoselement) {
+            actualProductos.add(element.getText());
+        }
+        return actualProductos;
+    }
 }
+
+
+
+
+
--- src/test/java/paginaproductos/pom/iniciarSesion_Test.java
+++ src/test/java/paginaproductos/pom/iniciarSesion_Test.java
@@ -10,40 +10,49 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class iniciarSesion_Test {
-   private WebDriver driver;
-   IniciarSesionPagina iniciarSesionPagina;
+    private WebDriver driver;
+    IniciarSesionPagina iniciarSesionPagina;
 
-   @Before
-    public void setUp(){
-       iniciarSesionPagina=new IniciarSesionPagina(driver);
-       driver=iniciarSesionPagina.chromeDriverConnection();
-       iniciarSesionPagina.visit("https://www.saucedemo.com/");
-   }
-   @After
-    public void tearDown(){
-       driver.quit();
-   }
-   @Test
+    @Before
+    public void setUp() {
+        iniciarSesionPagina = new IniciarSesionPagina(driver);
+        driver = iniciarSesionPagina.chromeDriverConnection();
+        iniciarSesionPagina.visit("https://www.saucedemo.com/");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
     public void test() throws InterruptedException {
-       iniciarSesionPagina.iniciarSesion();
-       Thread.sleep(2000);
-      assertTrue(iniciarSesionPagina.ingresoPagina());
-       iniciarSesionPagina.takeScreenshot("inicio de sesion");
-   }
-   @Test
+        iniciarSesionPagina.iniciarSesion();
+        Thread.sleep(2000);
+        assertTrue(iniciarSesionPagina.ingresoPagina());
+        //iniciarSesionPagina.takeScreenshot("inicio de sesion");
+    }
+
+    @Test
     public void test2() throws InterruptedException {
-       iniciarSesionPagina.iniciarSesionError();
-       assertEquals("Epic sadface: Username and password do not match any user in this service",iniciarSesionPagina.mensajeErrorLogeo());
-       iniciarSesionPagina.takeScreenshot("Contraseña incorrecta, Mensaje de error");
-   }
-   @Test
-    public void test3(){
-       iniciarSesionPagina.iniciarSesionError();
-       assertEquals(1,iniciarSesionPagina.xUsu());
-       assertEquals(1,iniciarSesionPagina.xPass());
-       assertEquals(1,iniciarSesionPagina.xMsjErrorr());
-       iniciarSesionPagina.takeScreenshot("Validación de X inicio con error");
-   }
+        iniciarSesionPagina.iniciarSesionError();
+        assertEquals("Epic sadface: Username and password do not match any user in this service", iniciarSesionPagina.mensajeErrorLogeo());
+        //iniciarSesionPagina.takeScreenshot("Contraseña incorrecta, Mensaje de error");
+    }
 
+    @Test
+    public void test3() {
+        iniciarSesionPagina.iniciarSesionError();
+        assertEquals(1, iniciarSesionPagina.xUsu());
+        assertEquals(1, iniciarSesionPagina.xPass());
+        assertEquals(1, iniciarSesionPagina.xMsjErrorr());
+        //iniciarSesionPagina.takeScreenshot("Validación de X inicio con error");
+    }
 
+    //Validación de numero de productos
+    @Test
+    public void test4() {
+        System.out.println("productos: "+iniciarSesionPagina.actualProductoss());
+
+    }
 }
